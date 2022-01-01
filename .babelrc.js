6791b616b08c6c997f3b4c09e9cc607425202c91
James Isaac <james@jamesisaac.com>
2022-01-01 12:51:06 +0000

    chore: upgrade libs

--- .babelrc.js
+++ .babelrc.js
@@ -4,19 +4,17 @@ module.exports = {
       '@babel/env',
       {
         targets: {
-          browsers: ['ie >= 11']
+          browsers: ['ie >= 11'],
         },
         exclude: ['transform-async-to-generator', 'transform-regenerator'],
         modules: false,
-        loose: true
-      }
+      },
     ],
     '@babel/react',
-    '@babel/flow'
+    '@babel/flow',
   ],
   plugins: [
     '@babel/proposal-object-rest-spread',
-    '@babel/proposal-class-properties',
     '@babel/transform-modules-commonjs',
-  ]
-}
+  ],
+};
